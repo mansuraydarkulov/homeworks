c9128c77af1df964d712d5aed231e622a0ef032c
mansuraydarkulov <mansur@turov.pro>
2022-05-17 19:59:07 +0300

    main

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,22 @@
-console.log('Hello World');
+console.log("Hello World");
 
 let test = 120;
 console.log(test);
 
 const test2 = 100;
 console.log(test2);
+
+result = test - test2;
+console.log(result);
+
+let name = prompt('Как вас зовут?');
+let modString = `Hello ${name}`;
+
+console.log(modString);
+
+const box = {
+    name: 'Max',
+    age: 28,
+    test: true,
+}
+console.log(box);
