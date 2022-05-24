cd0f38e8a095a770d0fa3a52eec2a217a639d29d
mansuraydarkulov <mansur@turov.pro>
2022-05-24 09:59:36 +0300

    main

--- javascript/index.js
+++ javascript/index.js
@@ -11,7 +11,7 @@ var str = '""';
 console.log(typeof num)
 console.log(typeof str)
 
-// 4
+// 4 и 5 
 var a = prompt("Введите число a");
 var b = prompt("Введите число b");
 var c = 0;
@@ -20,7 +20,7 @@ alert ("Cумма чисел: "+c);
 c = (Number(a) / Number(b));
 alert("Деление чисел: "+c); 
 
-// 5
+// 6
 var x = prompt("Введите число x");
 var y = prompt("Введите число y");
 var number = 0;
@@ -30,3 +30,26 @@ if (number % 2 == 0) {
 } else {
     alert("Число нечётное");
 }
+
+// 8
+var a = 3;
+var b = '3';
+var c = 0;
+var d = 'false';
+console.log(a === b);
+console.log(a == b);
+console.log(c === d);
+console.log(c == d);
+
+// 9
+var num = '11';
+var str = 'Hello';
+var str = 'Hello';
+var str1 = { a: 1 };
+var str2 = null;
+var str3 = false;
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof str1);
+console.log(typeof str2);
+console.log(typeof str3);
