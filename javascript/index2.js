e43c667aa22edbda2c6b56dc62358b1a94e9fd30
mansuraydarkulov <mansur@turov.pro>
2022-05-30 09:35:34 +0300

    main

--- javascript/index2.js
+++ javascript/index2.js
@@ -1,9 +1,11 @@
+//1
 function sum(a, b) {
     return a + b;
   }
 let result = sum(1, 2);
 console.log(result);  
 
+//2
 function compare(a, b) {
     if (a > b) {
         console.log("А больше Б");
@@ -12,4 +14,27 @@ function compare(a, b) {
     }
   }
 let resultСompare = compare(1, 2);
-console.log(resultСompare);  
+
+//3
+let calcSum = (a, b) => a + b;
+console.log(calcSum(2, 3))
+
+let calcSub = (a, b) => a - b;
+console.log(calcSub(4, 3))
+
+let calcMult = (a, b) => a * b;
+console.log(calcMult(2, 3))
+
+let calcDiv = (a, b) => a / b;
+console.log(calcDiv(6, 3))
+
+//4
+let userName = prompt('Как вас зовут');
+
+function showMessage() {
+
+  let message = 'Привет, ' + userName;
+  console.log(message);
+}
+
+showMessage();  
