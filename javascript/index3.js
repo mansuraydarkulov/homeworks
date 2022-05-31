bc5dc9025545df6c2d9cb7de21222cf02f5879d5
mansuraydarkulov <mansur@turov.pro>
2022-05-31 10:28:18 +0300

    main

--- javascript/index3.js
+++ javascript/index3.js
@@ -2,7 +2,16 @@ const buttonDec = document.querySelector('.buttonDec');
 const buttonSum = document.querySelector('.buttonSum');
 const span = document.querySelector('.value');
 
-buttonIncrement.addEventListoner('click', () => {
+const inputNumber = document.querySelector('.input-number')
+const buttonSubmit = document.querySelector('.button-submit')
+
+inputNumber.addEventListener('input', (event))
+
+buttonDec.addEventListener('click', {} => {
     let currentValue = span.textContent;
-    span.textContent = currentValue + 1;
+    span.textContent = parseInt(currentValue) + 1;
+})
+buttonSum.addEventListener('click', {} => {
+    let currentValue = span.textContent;
+    span.textContent = parseInt(currentValue) - 1;
 })
