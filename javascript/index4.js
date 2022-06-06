de8f0c0cf827a46cbd261d4d8d0662a31d96f475
mansuraydarkulov <mansur@turov.pro>
2022-06-06 10:51:35 +0300

    main

--- javascript/index4.js
+++ javascript/index4.js
@@ -2,9 +2,9 @@ function fib(n) {
     if(n<2) {
         return n;
     }
-    return fib(n -1) + fib(n -2);
+    return fib(n - 1) + fib(n - 2);
   }
-console.log(fib(5));  
+console.log(fib(10));  
 
 function fibonacci(num) {
     let a = 1;
