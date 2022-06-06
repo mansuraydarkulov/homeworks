de7da21acfabd42dfbb4a53e1e8cf2161b5324a7
mansuraydarkulov <mansur@turov.pro>
2022-06-06 10:24:06 +0300

    main

--- javascript/index4.js
+++ javascript/index4.js
@@ -1,6 +1,19 @@
 function fib(n) {
     return n <= 1 ? n : fib(n - 1) + fib(n - 2);
   }
+  console.log( fib(2) );  
+  console.log( fib(9) ); 
+
+function fib(n) {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  }
   
-  alert( fib(3) ); // 2
-  alert( fib(7) ); // 13
+  console.log( fib(3) ); // 2
+  console.log( fib(7) ); // 13
